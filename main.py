a962f7490bac3a8c6510c1aa52f36b93f8a0c893
Ritik <ritikpradhan20@gmail.com>
2023-11-27 13:33:35 +0530

    As part of my internship with InternArmy, I am actively involved in the development of a dynamic and interactive Online Quiz Platform using Python and Flask. This project showcases my skills in web development, database design, and problem-solving.

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    # Add more user-related fields as needed
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=False)
+    question_type = db.Column(db.String(20), nullable=False)
+    # Add more question-related fields as needed
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+    answer_content = db.Column(db.String(100), nullable=False)
+    # Add more answer-related fields as needed
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
